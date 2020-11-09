ed924d5dab67e5b7b3865b75f4f772001d090885
hongyue-qiu <994059641@qq.com>
2020-11-09 23:42:58 +0800

    practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = (input) => {
+    const { data, column } = input;
+    const keys = column.map((cn) => cn.name);
+    return data.map((values) => {
+        const obj = {};
+        keys.forEach((key, i) => {
+            obj[key] = values[i];
+        });
+
+        return obj;
+    });
+};
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections)=>{
+    for (let i = 0; i < sections.length; i++) {
+        var index = sections[i].index+i;
+        items.splice(index, 0, sections[i].content);
+    }
+    return items;
+}
 export { inject };
